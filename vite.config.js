0041e752044f5c542e29da3c39c0797038911e8f
victor <weider.hsu@gmail.com>
2026-03-02 22:39:29 +0800

    Set explicit base path for GitHub Pages and Service Worker compatibility

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-    base: './',
+    base: '/SheetMusic_Viewer/',
     plugins: [
         VitePWA({
             registerType: 'autoUpdate',
